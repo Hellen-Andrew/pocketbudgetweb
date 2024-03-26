fbee473f8e8b18e5285151b39de304fee2671a48
Helllen Andrew <ahellen043@gmail.com>
2024-03-26 11:46:40 +0300

    install tailwind

--- pocketbudgeting/src/Components/PinkNavBar.js
+++ pocketbudgeting/src/Components/PinkNavBar.js
@@ -1,7 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PinkNavBar = () => {
-  return <div>Pocket</div>;
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleNavbar = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white dark:bg-gray-900 pb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+            <div>
+              <a href="#" className="flex items-center">
+                <span className="self-center text-2xl ml-2 font-semibold whitespace-nowrap dark:text-white">
+                  Pocket
+                </span>
+              </a>
+            </div>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={toggleNavbar}
+            >
+              {!showMenu ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-list"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-x-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div
+            className={
+              showMenu
+                ? "lg:flex lg:flex-gro items-center"
+                : "hidden lg:flex lg:flex-grow items-center"
+            }
+            class="lg:flex lg:flex-grow items-center"
+          >
+            <ul className="flex flex-col lg:flex-row list-none ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75" />
+                  <span className="ml-2">vcard</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#"
+                >
+                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75" />
+                  <span className="ml-2">market place</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#"
+                >
+                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75" />
+                  <span className="ml-2">literature</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#"
+                >
+                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75" />
+                  <span className="ml-2">economy</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#"
+                >
+                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75" />
+                  <span className="ml-2">carriers</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default PinkNavBar;
